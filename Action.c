3143e656674706c7da3a1febcdead8e6aac7448a
Emmanuel ASIFIWE <emmanuel2koestner2gmail.com>
2023-01-13 09:41:36 -0500

    This is the first commit which indicate the starting of performance test on my personal brand website.

---
+++ Action.c
@@ -0,0 +1,58 @@
+//   *****************************************************************************************************************************************
+//   ****   PLEASE NOTE: This is a READ-ONLY representation of the actual script. For editing please press the "Develop Script" button.   ****
+//   *****************************************************************************************************************************************
+
+Action()
+{
+	truclient_step("1", "Navigate to 'https://emmanuel262.git...manuel-asifiwe/'", "snapshot=Action_1.inf");
+	truclient_step("2", "Click on About link", "snapshot=Action_2.inf");
+	truclient_step("3", "Click on Read more button", "snapshot=Action_3.inf");
+	truclient_step("4", "Click on Resume link", "snapshot=Action_4.inf");
+	truclient_step("5", "Activate tab # 2", "snapshot=Action_5.inf");
+	truclient_step("6", "Close Tab active tab", "snapshot=Action_6.inf");
+	truclient_step("7", "Activate tab # 1", "snapshot=Action_7.inf");
+	truclient_step("8", "Click on RBA intern button", "snapshot=Action_8.inf");
+	truclient_step("9", "Click on Click to learn more of... button", "snapshot=Action_9.inf");
+	truclient_step("10", "Click on image (1) image", "snapshot=Action_10.inf");
+	truclient_step("11", "Click on View Certificate link", "snapshot=Action_11.inf");
+	truclient_step("12", "Activate tab # 2", "snapshot=Action_12.inf");
+	truclient_step("13", "Close Tab active tab", "snapshot=Action_13.inf");
+	truclient_step("14", "Activate tab # 1", "snapshot=Action_14.inf");
+	truclient_step("15", "Click on Self Learning experience button", "snapshot=Action_15.inf");
+	truclient_step("16", "Click on Click to learn more about... button", "snapshot=Action_16.inf");
+	truclient_step("17", "Click on button (2) button", "snapshot=Action_17.inf");
+	truclient_step("18", "Click on Andela ATLP button", "snapshot=Action_18.inf");
+	truclient_step("19", "Click on Click to learn more about... button", "snapshot=Action_19.inf");
+	truclient_step("20", "Click on image (2) image", "snapshot=Action_20.inf");
+	truclient_step("21", "Click on Read more link", "snapshot=Action_21.inf");
+	truclient_step("22", "Click on Click here link", "snapshot=Action_22.inf");
+	truclient_step("23", "Activate tab # 2", "snapshot=Action_23.inf");
+	truclient_step("24", "Mouse Over", "snapshot=Action_24.inf");
+	{
+		truclient_step("24.1", "Move mouse over element (1)", "snapshot=Action_24.1.inf");
+	}
+	truclient_step("25", "Click on Play Overlay button", "snapshot=Action_25.inf");
+	truclient_step("26", "Click on This video format is...", "snapshot=Action_26.inf");
+	truclient_step("28", "Close Tab active tab", "snapshot=Action_28.inf");
+	truclient_step("29", "Activate tab # 1", "snapshot=Action_29.inf");
+	truclient_step("30", "Click on Access codes from github link", "snapshot=Action_30.inf");
+	truclient_step("31", "Activate tab # 2", "snapshot=Action_31.inf");
+	truclient_step("32", "Click on Web Application CMMS link", "snapshot=Action_32.inf");
+	truclient_step("34", "Close Tab active tab", "snapshot=Action_34.inf");
+	truclient_step("35", "Activate tab # 1", "snapshot=Action_35.inf");
+	truclient_step("36", "Click on white mode", "snapshot=Action_36.inf");
+	truclient_step("37", "Click on black mode", "snapshot=Action_37.inf");
+	truclient_step("38", "Click on Home link", "snapshot=Action_38.inf");
+	truclient_step("39", "Click on Your name textbox", "snapshot=Action_39.inf");
+	truclient_step("40", "Type emmanuel.asifiwe@microfocus.com in Your name textbox", "snapshot=Action_40.inf");
+	truclient_step("41", "Click on Your name textbox", "snapshot=Action_41.inf");
+	truclient_step("42", "Type Emmanuel ASIFIWE in Your name textbox", "snapshot=Action_42.inf");
+	truclient_step("43", "Type emmanuel.asifiwe@microfocus.com in Enter your email emailbox", "snapshot=Action_43.inf");
+	truclient_step("44", "Click on Contact us The reasons... textbox", "snapshot=Action_44.inf");
+	truclient_step("45", "Type This is an amazing testi...onal portifolio in Contact us The reasons... textbox", "snapshot=Action_45.inf");
+	truclient_step("46", "Click on Send your message button", "snapshot=Action_46.inf");
+	truclient_step("47", "Click on Back Home link", "snapshot=Action_47.inf");
+	truclient_step("48", "Click on focusable (1)", "snapshot=Action_48.inf");
+
+	return 0;
+}
